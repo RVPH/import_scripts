e2939adf1bf262bbd63f2ecccf2e49a92eeea1f1
Alexander Miskaryan <alexmisk@Alexanders-MacBook-Pro.local>
2020-07-20 10:30:54 +0300

    Use python3

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 [ -z $(which brew)  ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 [ -z $(which python3)  ] && brew install python3
 [ -z $(which direnv)  ] && brew install direnv
-[ -z $(which virtualenv)  ] && python -m pip install --user virtualenv
+[ -z $(which virtualenv)  ] && python3 -m pip install --user virtualenv
 
 echo "Paste mongodb connection string here:"
 read mongo_conn_string

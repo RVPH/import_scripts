f7ae934c5a90844ae2efec9e71e93256ecbe6d15
akademik <pvv2606@gmail.com>
2020-07-21 10:30:07 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 [ -z $(which brew)  ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 [ -z $(which python3)  ] && brew install python3
 [ -z $(which direnv)  ] && brew install direnv
-[ -z $(which virtualenv)  ] && pip3 install virtualenv
+[ -z $(which virtualenv)  ] && python3 -m pip install virtualenv
 
 echo "--------------------------------------"
 echo "Paste mongodb connection string here:"
@@ -14,7 +14,7 @@ read mongo_conn_string
 echo "export MONGO_DEV_URI='$mongo_conn_string'" > .envrc
 
 if [ ! -d ".venv" ]; then
-  virtualenv .venv -p python3
+  python3 -m venv .venv
 fi
 
 .venv/bin/pip install -r requirements.txt
